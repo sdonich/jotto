58f253750b414d237e737910190639a8bb9bbbc2
sdonich <sdonich@gmail.com>
2019-08-25 17:17:31 +0200

    Upgrade GuessedWords

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,13 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const GuessedWords = props => {
-  const contents = props.guessedWords.length === 0 ?
-    <span data-test="guess-instructions">
-      Try to guess the secret word!
-    </span>
-    :
-    null;
-  
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">
+        Try to guess the secret word!
+      </span>
+    );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, i) => {
+      return (
+        <tr key={i} data-test="guessed-word">
+          <td>{word.guessedWord}</td>
+          <td>{word.letterMatchCount}</td>
+        </tr>
+      );
+    });
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Marching letters</th>
+            </tr>
+          </thead>
+          <tbody>
+            {guessedWordsRows}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
   return (
     <div data-test="component-guessed-words">
       {contents}
