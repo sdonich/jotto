143ebc29b612cb19caa00156793eac333669dfbb
sdonich <sdonich@gmail.com>
2019-08-25 08:31:19 +0200

    Add setupTests
    We create setupTests for configure setup of testing. All we upgrade
    congrats.test.js

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Congrats = props => {
   return (
@@ -11,6 +12,10 @@ const Congrats = props => {
       :
       <div data-test="component-congrats" />
   );
-}
+};
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired
+};
 
 export default Congrats;
