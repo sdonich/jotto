ae85821cc8da33fb7d47ca77069ab0d82c0fad4b
sdonich <sdonich@gmail.com>
2019-08-25 20:37:24 +0200

    Set up component  and storeFactory test

---
+++ src/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {connect } from 'react-redux';
+
+class Input extends React.Component {
+  render() {
+    return <div><button /></div>
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+}
+
+export default connect(mapStateToProps)(Input);
