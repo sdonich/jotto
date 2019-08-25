e2b189a0af22aec1033f93d3697966936ecc0844
sdonich <sdonich@gmail.com>
2019-08-25 19:42:51 +0200

    Write code test for success reducer

---
+++ src/reducers/success.test.js
@@ -0,0 +1,11 @@
+import {actionTypes} from '../actions';
+import success from './success';
+
+test('return default initial state of "false" when no action is passed', () => {
+  const newState = success(undefined, {});
+  expect(newState).toBe(false);
+});
+test('return state of true upon receining an action of type "CORRECT_GUESS"', () => {
+  const newState = success(undefined, {type: actionTypes.CORRECT_GUESS});
+  expect(newState).toBe(true);
+});
