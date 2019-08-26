0b0d94b76d1847768d9276895aba92e5a79c0482
Sergei <sdonich@gmail.com>
2019-08-26 16:50:12 +0200

    Set up guessWords action and reducer

---
+++ src/integration.test.js
@@ -0,0 +1,22 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispather', () => {
+  describe('no guesse words', () => {
+    test('updates state correctly for unsuccessful guess', () => {
+
+    });
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+
+  describe('some guesse words', () => {
+    test('updates state correctly for unsuccessful guess', () => {
+
+    });
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+});
