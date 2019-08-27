403cd51cd39fc5bd2d3f3eef0d6842da752c989b
Sergei <sdonich@gmail.com>
2019-08-27 16:41:26 +0200

    Fix

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,5 @@
 export function getLetterMatchCount(guessedWord, secretWord) {
   const secretLetterSet = new Set(secretWord.split(''));
   const guessedLetterSet = new Set(guessedWord.split(''));
-  return [... secretLetterSet].filter(letter => guessedLetterSet.has(letter)).length;
+  return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter)).length;
 };
